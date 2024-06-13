817757928c044d6d62128eb8b20fe4d22accc50b
Dmitry <flaserhost@gmail.com>
2024-06-13 16:04:30 +0300

    init

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector(".modal");
+modal.style.display = "flex";
+/*"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        const modal = document.querySelector(".modal");
+        modal.style.display = "flex";
+    }, 15000);
+});*/
