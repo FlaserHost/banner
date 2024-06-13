79141ff9eec2d317f5cbb02408ae46450bc2846d
Dmitry <flaserhost@gmail.com>
2024-06-13 16:15:41 +0300

    init

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        const modal = document.querySelector('.modal');
-        modal.style.display = 'flex';
+    if (!sessionStorage.bannerWatched) {
+        setTimeout(() => {
+            const modal = document.querySelector('.modal');
+            modal.style.display = 'flex';
 
-        const closeBtn = modal.querySelector('#banner-close-btn');
-        closeBtn.addEventListener('click', e => e.target.closest('.modal').remove());
-    }, 15000);
+            const closeBtn = modal.querySelector('#banner-close-btn');
+            closeBtn.addEventListener('click', e => {
+                e.target.closest('.modal').remove();
+                sessionStorage.bannerWatched = true;
+            });
+        }, 15000);
+    }
 });
